aba52a8735d8df064346ff2b189cdc66d1b4cfd0
ksubin98 <subin.k1229@gmail.com>
2022-02-25 19:37:57 +0900

    Router

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import styles from './app.module.css';
 import Login from './components/page/login/login';
+import Signup from './components/page/signup/signup';
 
-function App() {
+function App({user}) {
 
   return (
   <div className={styles.app}>
-    <Login />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login user={user}/>} />
+        <Route path="/signup" element={<Signup user={user}/>} />
+      </Routes>
+    </Router>
   </div>
   )
 }
