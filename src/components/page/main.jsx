795109e6d794e66f5754081e7d09d2a60b8aec95
ksubin <subin.k1229@gmail.com>
2022-08-12 12:38:42 +0900

    [design] 소개추가

---
+++ src/components/page/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Main = (props) => {
+
+
+  return(
+  <>
+  <p>안녕하세요.</p>
+  <p>마케팅하는 프론트엔드 개발자 권수빈입니다.</p>
+  </>
+  )
+}
+
+export default Main;
+
+
