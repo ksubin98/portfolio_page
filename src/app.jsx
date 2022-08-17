ca4361b1c7827abeb2bb205445273701020de748
ksubin <subin.k1229@gmail.com>
2022-08-17 19:04:53 +0900

    [design] contact 화면 틀

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './app.css';
 import About from './components/page/about';
+import Contact from './components/page/contact';
 import Main from './components/page/main';
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
           </Route>
           <Route path="/About">
             <About />
+          </Route>
+          <Route path="/Contact">
+            <Contact />
           </Route>
         </Switch>
       </Router>
---
+++ src/components/page/contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Contact = (props) => {
+  
+  return(
+    <>
+    {/*각각에 맞는 아이콘 추가 후 링크 설정 */}
+    <h2>🙋‍♀️많은 연락주세요🙋‍♀️</h2>
+    <h3>SNS</h3>
+    <li>
+      <button>github</button>
+      <button>Dev-blog</button>
+      <button>Daily-blog</button>
+    </li>
+    <h3>Email</h3>
+      <button>subin.k1229@gmail.com</button>
+    </>
+  )
+}
+
+export default Contact;
